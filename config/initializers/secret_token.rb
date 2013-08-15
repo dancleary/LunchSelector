3efba5b73c897a86dc60fa6719853b6ab5597992
Daniel Cleary <dan.cleary@sendgrid.com>
2013-08-15 11:29:27 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LunchSelector::Application.config.secret_key_base = '1dccbbb229248f3034a63d4a3d0f690b23957aef802245c3152c37b27dcc6d818d1da95c0bc9ef524a75b24c724e03f790658972d7742a0b86e3a98acd08e27e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LunchSelector::Application.config.session_store :cookie_store, key: '_LunchSelector_session'
